2d25c317b08a71066afa3948a3a2a2bf0c288fd9
Vadym <zinchenkovadym@gmail.com>
2022-04-17 20:36:25 +0300

    code improvement

--- src/components/header/form/AddCity.jsx
+++ src/components/header/form/AddCity.jsx
@@ -1,35 +1,44 @@
-import './../../../App.css'
-import './AddCity.css'
+import React from 'react';
+import './../../../App.css';
+import './AddCity.scss';
+import PropTypes from 'prop-types';
 
 const AddCity = (props) => {
-
-    window.onclick = (e) => props.cityNotFound(false)
-
-    return (
-        <div className='form__add-city'>
-            <h2 className={`not_found${props.cityNotFoundBool ? ' active' : ''}`}>City not found</h2>
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                props.getCityWeather(props.cityName)
-            }}>
-                <input
-                    placeholder='City name...'
-                    type='text'
-                    onChange={(e) => {
-                        props.cityNotFound(false)
-                        props.changeCity(e.target.value)
-                        console.log(props.cityName)
-                    }}
-                    value={props.cityName}
-                    required
-                />
-                <button type='submit'
-                >
+  return (
+    <div className='form__add-city'>
+      <h2 className={`not_found  ${props.cityNotFoundBool ? ' active' : ''}`}>
+        City not found
+      </h2>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        props.getCityWeather(props.cityName);
+      }}>
+        <input
+          placeholder='City name...'
+          type='text'
+          onChange={(e) => {
+            props.cityNotFound(false);
+            props.changeCity(e.target.value);
+            console.log(props.cityName);
+          }}
+          value={props.cityName}
+          required
+        />
+        <button type='submit'
+        >
                     Search
-                </button>
-            </form>
-        </div>
-    );
-}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+AddCity.propTypes = {
+  cityNotFoundBool: PropTypes.bool,
+  cityName: PropTypes.string,
+  getCityWeather: PropTypes.func,
+  cityNotFound: PropTypes.func,
+  changeCity: PropTypes.func,
+};
 
 export default AddCity;
